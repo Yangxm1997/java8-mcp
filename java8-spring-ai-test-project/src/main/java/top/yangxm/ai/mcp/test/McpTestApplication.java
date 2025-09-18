76057b1c1801c13568836589f25c5025d7b211bd
Yangxm <yangxianming1997@foxmail.com>
2025-09-18 18:46:49 +0800

    调整依赖

---
+++ java8-spring-ai-test-project/src/main/java/top/yangxm/ai/mcp/test/McpTestApplication.java
@@ -0,0 +1,11 @@
+package top.yangxm.ai.mcp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(McpTestApplication.class, args);
+    }
+}
